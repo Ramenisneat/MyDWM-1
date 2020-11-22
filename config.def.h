90c99b881dabf649aa9ae6de8f3faa4f8d38a0fb
Cody Quinn <codyquinn1122@gmail.com>
2020-11-21 19:01:12 -0600

    :rice: Riced the shit outa DWM (massive creddit to u/fabricio7p for this though)

--- config.def.h
+++ config.def.h
@@ -1,27 +1,68 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+static const char *fonts[]          = { "Fantasque Sans Mono Nerd Font:size=14:antialias=true:autohint=true", };
+static const char dmenufont[]       = "Fira Sans:size=14:antialias=true:autohint=true";
+
+// Colors
+static const char col_gb_black[]	= "#131515";
+static const char col_gb_bg[]		= "#1d2021";
+static const char col_gb_bgm[]      = "#282828";
+static const char col_gb_bgs[]      = "#32302f";
+static const char col_gb_fg[]       = "#fbf1c7";
+
+static const char col_gb_red1[]     = "#cc241d";
+static const char col_gb_red2[]     = "#fb4934";
+
+static const char col_gb_green1[]   = "#98971a";
+static const char col_gb_green2[]   = "#b8bb26";
+
+static const char col_gb_yellow1[]  = "#d79921";
+static const char col_gb_yellow2[]  = "#fabd2f";
+
+static const char col_gb_blue1[]    = "#458588";
+static const char col_gb_blue2[]    = "#83a598";
+
+static const char col_gb_purple1[]  = "#b16286";
+static const char col_gb_purple2[]  = "#83869b";
+
+static const char col_gb_aqua1[]    = "#689d6a";
+static const char col_gb_aqua2[]    = "#8ec07c";
+
+static const char col_gb_gray1[]    = "#a89984";
+static const char col_gb_gray2[]    = "#928374";
+static const char col_gb_gray3[]    = "#665c54";
+static const char col_gb_gray4[]    = "#3c3836";
+static const char col_gb_gray5[]    = "#a89984";
+
+
+static const char col_gb_orange1[]  = "#d65d0e";
+static const char col_gb_orange2[]  = "#fe8019";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm]     = { col_gb_gray2,   col_gb_bgm,     col_gb_bg },
+	[SchemeSel]      = { col_gb_orange2, col_gb_bgm,     col_gb_black },
+	[SchemeSel2]     = { col_gb_fg,      col_gb_bg,     col_gb_bg },
+	[SchemeUrgent]   = { col_gb_fg,      col_gb_bg,     col_gb_bg },
+	[SchemeTitle]    = { col_gb_fg,      col_gb_bgs,    col_gb_bg },
+	[SchemeLt]       = { col_gb_orange2, col_gb_bgm,    col_gb_bg },
+	[SchemeNotify]   = { col_gb_red1,    col_gb_bg,     col_gb_bg },
+	[SchemeStatus]   = { col_gb_aqua2,   col_gb_bg,     col_gb_bg },
+	[SchemeIndOff]   = { col_gb_green2,  col_gb_bg,     col_gb_bg },
+	[SchemeIndOn]    = { col_gb_blue2,   col_gb_bg,     col_gb_bg },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "" };
+static const char *tagsalt[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,8 +70,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	{ "kcalc",	  NULL,		  NULL,		  0, 			1,			 1,			  -1 },
+	{ "spectacle",NULL,		  NULL,		  0, 			1, 			 1, 		  -1 },
 };
 
 /* layout(s) */
@@ -40,13 +81,16 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "﩯 ",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define SUPERMOD Mod4Mask|Mod1Mask
+#define ALTMOD Mod1Mask
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -59,12 +103,24 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	// Applications
+	{ MODKEY|ShiftMask,				XK_d,	   spawn,		   SHCMD("discord") }
+	{ MODKEY|ShiftMask,				XK_e,	   spawn,		   SHCMD("qutebrowser") }
+	{ MODKEY|ShiftMask,				XK_h,	   spawn,		   SHCMD("kitty htop") }
+	{ MODKEY|ShiftMask,				XK_g,	   spawn,		   SHCMD("steam") }
+	{ MODKEY|ShiftMask,				XK_r,	   spawn,		   SHCMD("dolphin") }
+
+	// Screenshot
+	{ MODKEY|ShiftMask,				XK_s,	   spawn,		   SHCMD("spectacle -bnrc") }
+
+	// Utilities (dmenu & terminal)
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "~/.dwm/statusbar/_traffic.sh",		1,			0},
+	{"", "~/.dwm/statusbar/_wifi.sh",			1,			0},
+	{"", "~/.dwm/statusbar/_audio.sh",			0,			10},
+	{"", "~/.dwm/statusbar/_cpu.sh",			1,			0},
+	{"", "~/.dwm/statusbar/_battery.sh",		60,			0},
+	{"", "~/.dwm/statusbar/_date.sh",			3600,		1},
+	{"", "~/.dwm/statusbar/_displaytime.sh",	1,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = "\0";
